4a9a529c9a8e0278d1064de8ffd7ea6c78c7a9c9
AigerimMaden <m_s_g_t@mail.ru>
2020-09-03 16:34:15 +0000

    adding

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -21,7 +21,7 @@ if (nodeIP?.trim()) {
             }
             stage("Install Terraform"){
 
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install -y wget unzip'
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install  wget unzip -y'
 
                 sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} wget https://releases.hashicorp.com/terraform/0.13.1/terraform_0.13.1_linux_amd64.zip'
                 sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} unzip terraform_0.13.1_linux_amd64.zip'
